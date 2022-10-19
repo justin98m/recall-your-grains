07eb71ec570c1f3edd0a31fecf349077f007a3df
Justin C <justin1998m1@gmail.com>
2022-10-18 22:10:32 -0400

    simplified ui added js to react to add task btn click

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-import express from 'express'
-import nunjucks from 'nunjucks'
-import bodyParser from 'body-parser'
+import express from 'express';
+import nunjucks from 'nunjucks';
+import bodyParser from 'body-parser';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+//path to index file
+const __filename = fileURLToPath(import.meta.url);
+//path to head server directory  of index file
+const __dirname = path.dirname(__filename);
+//what does express() function do??
 const app = express();
 //what does this do?
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}));
 //tell express where static files are
 app.use(express.static(__dirname + '/public'));
 const port = 3000;
@@ -13,19 +21,14 @@ nunjucks.configure('./public/views', {
 	express: app
 });
 app.get('/',(req,res) => {
-
-	res.render('home.html',	data = {
-		css: 'home.css'
-	})
-})
+	res.render('home.html');
+});
 
 //Getting Post Data
 app.post('/addTask',(req,res) =>{
 	let data = {
 	}
 
-})
-
-
+});
 app.listen(port,() => {
-})
+});
