f9a330706ee6cceb4bae48aceaf50006e3a0b1b3
Justin C <justin1998m1@gmail.com>
2022-10-25 00:06:45 -0400

    reorganize home structure + extracted selectedTask function

--- public/scripts/helper.js
+++ public/scripts/helper.js
@@ -1,5 +1,15 @@
 const MAX_TASKNAME_LENGTH = 75;
 
-export default function validTaskName(taskName){
+export function validTaskName(taskName){
   return taskName.length > 0 && taskName.length <= MAX_TASKNAME_LENGTH;
+}
+
+//return all the task containers with a checked checkbox
+export function selectedTasks(){
+  //returns array of task containers
+  let tasks = Object.values(document.querySelectorAll('.taskContainer'));
+  return tasks.filter(task => {
+    let taskStatus = task.querySelector('.taskCheckbox');
+    return taskStatus.checked;
+  })
 }
